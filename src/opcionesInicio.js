815c10922120349b0d22c0cb60a4688ad5f0faa4
Mariano Camposeco <mariano1941@outlook.es>
2021-12-09 15:46:17 -0600

    Agregamos condicionales para restriccion de datos

--- src/opcionesInicio.js
+++ src/opcionesInicio.js
@@ -13,20 +13,39 @@ const cui = document.getElementById("cuiLlamar");
 const departamento = document.getElementById("departamentoLlamar");
 //Funciones
 function IniciarSesion(){
-    console.log("Nombre: "+nombre.value);
-    console.log("Contraseña: "+contraseña.value)
+    if(nombre.value==""||contraseña.value==""){
+        console.log("Primero llena todos los datos");
+    }else{
+        console.log("Nombre: "+nombre.value);
+        console.log("Contraseña: "+contraseña.value);
+    }
+    
 }
 
 function Registrar(){
-    console.log("Correo: "+correo.value);
-    console.log("Nombre: "+nombreRegistro.value);
-    console.log("Contraseña: "+contraseñaRegistro.value);
-    console.log("Contraseña confirmar: "+confirmarContraseñaRegistro.value);
+    if(contraseñaRegistro.value==confirmarContraseñaRegistro.value){
+        if(correo.value==""||nombreRegistro.value==""||contraseñaRegistro.value==""||confirmarContraseñaRegistro.value==""){
+            console.log("Primero llena todos los datos");
+        }else{
+            console.log("Correo: "+correo.value);
+            console.log("Nombre: "+nombreRegistro.value);
+            console.log("Contraseña: "+contraseñaRegistro.value);
+            console.log("Contraseña confirmar: "+confirmarContraseñaRegistro.value);
+        }
+        
+    }else{   
+        console.log("Error, contraseñas no son iguales");
+    }
 }
 
 function IngresarCliente(){
-    console.log("nombre "+nombreCliente.value);
-    console.log("apellido "+apellido.value);
-    console.log("cui "+cui.value);
-    console.log("departamento "+departamento.value);
+    if(nombreCliente.value==""||apellido.value==""||cui.value==""||departamento.value==""){
+        console.log("Primero llena todos los datos");
+    }else{
+        console.log("nombre "+nombreCliente.value);
+        console.log("apellido "+apellido.value);
+        console.log("cui "+cui.value);
+        console.log("departamento "+departamento.value);
+    }
+    
 }
